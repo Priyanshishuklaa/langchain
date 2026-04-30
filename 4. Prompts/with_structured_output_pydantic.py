8141e8c305bf27955784a2610859cfa31d8f2910
Priyanshishuklaa <priyanshishukla250604@gmail.com>
2026-04-30 08:20:39 +0530

    Updated Code

--- 4. Prompts/with_structured_output_pydantic.py
+++ 4. Prompts/with_structured_output_pydantic.py
@@ -1,8 +1,9 @@
 from langchain_openai import AzureChatOpenAI
-
+from pydantic import BaseModel, Field
+from dotenv import load_dotenv
 from typing import TypedDict, Annotated, Optional, Literal
 import os
-
+load_dotenv(override=True)
 
   
 AZURE_OPENAI_DEPLOYMENT = os.getenv("AZURE_OPENAI_DEPLOYMENT")
@@ -12,14 +13,20 @@ AZURE_OPENAI_API_VERSION = os.getenv("AZURE_OPENAI_API_VERSION")
 model=AzureChatOpenAI(azure_deployment=AZURE_OPENAI_DEPLOYMENT, api_version=AZURE_OPENAI_API_VERSION)
 
 #schema
-
+class Review(BaseModel):
+    key_themes: list[str]=Field(description="Write down all the key themes discussed in the review in a list")
+    summary: str=Field(description="A brief summary of the review")
+    sentiment: Literal['pos','neg']=Field(description="Return sentiment of the review, either 'pos' or 'neg'")
+    pros: Optional[list[str]]=Field(description="Write down all the pros inside a list")
+    cons: Optional[list[str]]=Field(description="Write down all the cons inside a list")
+    name: Optional[str]=Field(description="Write the name of the reviewer ")
 
 s=model.with_structured_output(Review)
 
-res=s.invoke("The hardware is great, but the software feels bloated. There are too many pre-installed apps that I can't remove. Also, the UI looks outdated compared to other brands. Hoping for a software update to fix this.")
-
+res=s.invoke("The hardware is great, but the software feels bloated. There are too many pre-installed apps that I can't remove. Also, the UI looks outdated compared to other brands. Hoping for a software update to fix this. Reviewed by Priyanshi")
+print('--------------------------------------')
 print(res)
 print('--------------------------------------')
-print(res['summary'])
+print(res.key_themes)
 print('--------------------------------------')
-print(res['sentiment'])
+print(res.sentiment)
--- 4. Prompts/with_structured_output_typedict.py
+++ 4. Prompts/with_structured_output_typedict.py
@@ -5,18 +5,20 @@ import os
 
 load_dotenv()
 
-AZURE_OPENAI_API_KEY = os.getenv("AZURE_OPENAI_API_KEY")
-AZURE_OPENAI_ENDPOINT = os.getenv("AZURE_OPENAI_ENDPOINT")  
 AZURE_OPENAI_DEPLOYMENT = os.getenv("AZURE_OPENAI_DEPLOYMENT")
 AZURE_OPENAI_API_VERSION = os.getenv("AZURE_OPENAI_API_VERSION")
 
 
-model=AzureChatOpenAI(azure_deployment=AZURE_OPENAI_DEPLOYMENT, api_version=AZURE_OPENAI_API_VERSION)
-
+model=AzureChatOpenAI(azure_deployment="gpt-4.1-mini", api_version=AZURE_OPENAI_API_VERSION)
 #schema
+# schema
 class Review(TypedDict):
+
+    key_themes: Annotated[list[str], "Write down all the key themes discussed in the review in a list"]
     summary: Annotated[str, "A brief summary of the review"]
-    sentiment: Annotated[Literal["positive", "negative", "neutral"], "The sentiment of the review"]
+    sentiment: Annotated[Literal["pos", "neg"], "Return sentiment of the review either negative, positive or neutral"]
+    pros: Annotated[Optional[list[str]], "Write down all the pros inside a list"]
+    cons: Annotated[Optional[list[str]], "Write down all the cons inside a list"]
 
 s=model.with_structured_output(Review)
 
