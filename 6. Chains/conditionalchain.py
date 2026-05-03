72d549c4f31d31cdfefb793f5ea5c46f5a4b5cad
Priyanshishuklaa <priyanshishukla250604@gmail.com>
2026-05-03 20:42:32 +0530

    Added conditionchain.py

---
+++ 6. Chains/conditionalchain.py
@@ -0,0 +1,50 @@
+from langchain_openai import AzureChatOpenAI
+from langchain_core.prompts import PromptTemplate, MessagesPlaceholder
+from langchain_core.output_parsers import PydanticOutputParser, StrOutputParser
+from pydantic import BaseModel, Field
+from dotenv import load_dotenv
+from typing import Literal
+from langchain_core.runnables import RunnableParallel, RunnableBranch, RunnableLambda
+
+load_dotenv(override=True)
+
+model= AzureChatOpenAI(azure_deployment="gpt-4.1-mini",  api_version="2024-05-01-preview")
+
+parser=StrOutputParser()
+
+class Feedback(BaseModel):
+
+    sentiment: Literal['positive', 'negative'] = Field(description='Give the sentiment of the feedback')
+
+
+parser2 = PydanticOutputParser(pydantic_object=Feedback)
+
+prompt1 = PromptTemplate(
+    template='Classify the sentiment of the following feedback text into postive or negative \n {feedback} \n {format_instruction}',
+    input_variables=['feedback'],
+    partial_variables={'format_instruction':parser2.get_format_instructions()}
+)
+
+classifier_chain = prompt1 | model | parser2
+
+prompt2 = PromptTemplate(
+    template='Write an appropriate response to this positive feedback \n {feedback}',
+    input_variables=['feedback']
+)
+
+prompt3 = PromptTemplate(
+    template='Write an appropriate response to this negative feedback \n {feedback}',
+    input_variables=['feedback']
+)
+
+branch_chain = RunnableBranch(
+    (lambda x:x.sentiment == 'positive', prompt2 | model | parser),
+    (lambda x:x.sentiment == 'negative', prompt3 | model | parser),
+    RunnableLambda(lambda x: "could not find sentiment")
+)
+
+chain = classifier_chain | branch_chain
+
+print(chain.invoke({'feedback': 'This is a beautiful phone'}))
+
+chain.get_graph().print_ascii()
